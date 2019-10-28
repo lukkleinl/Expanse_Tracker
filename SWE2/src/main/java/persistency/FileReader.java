ff5530bec89a2b9bcca889ebb2243cb726597e9c
paul kraft <paul.kraft@riseup.net>
2019-10-28 23:50:05 +0100

    implemented basic File IO functionality

--- SWE2/src/main/java/persistency/FileReader.java
+++ SWE2/src/main/java/persistency/FileReader.java
@@ -1,12 +1,56 @@
 package persistency;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileReader {
 	private String fileName;
-	
-	public List<Object> readFile() {
+
+    public FileReader(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public List<Object> readFile() {
 		return new ArrayList<>();
+	}
+
+	private void Deserialize(){
+
+		File file = new File(fileName);
+
+		try{
+
+			if(file.createNewFile()){
+                List<? extends Object> list = new ArrayList<>(); //TODO if file doesnt Exist this Data ( 'list' ) gets written to file.....!
+                FileWriter writer = new FileWriter(); //TODO
+                writer.writeFile(list); // not the fastest but new File Writer only gets created if and if only file doesnt exist, doesnt happen often!
+            }
+
+
+        }catch (Exception e) {
+			// TODO EXCEPTION HANDLING
+		}
+
+
+		try {
+
+
+
+			FileInputStream inputStream = new FileInputStream(fileName);
+			ObjectInputStream objinputstream = new ObjectInputStream(inputStream);
+
+			//" When read they need to be cast to the expected type. " https://docs.oracle.com/javase/9/docs/api/java/io/ObjectInputStream.html
+            List<? extends Object> returnlist = new ArrayList<>();
+
+			inputStream.close();
+			objinputstream.close();
+
+		} catch (Exception e) {
+			// TODO EXCEPTION HANDLING
+		}
+
 	}
 }
