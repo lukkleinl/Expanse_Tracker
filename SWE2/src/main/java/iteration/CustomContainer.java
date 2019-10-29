c3d6b25166c2e10efa3e2f1f9335c5016ae7cddf
Michael Watholowitsch <a01613664@unet.univie.ac.at>
2019-10-29 11:35:26 +0100

    Disabled the remove() Method of this class' Iterator.

--- SWE2/src/main/java/iteration/CustomContainer.java
+++ SWE2/src/main/java/iteration/CustomContainer.java
@@ -2,17 +2,18 @@ package iteration;
 
 import java.util.Iterator;
 
-/** * 
+/**
  * Defines the necessary functionality of all CustomContainers.
- * 
+ *
  * @param <T> Element-Type to be stored
- * 
+ *
  * @author Michael Watholowitsch
  */
 public interface CustomContainer<T> {
-	/**
-	 * Creates and returns a CustomIterator for this CustomContainer.
-	 * @return A CustomIterator for this CustomContainer.
-	 */
-	public Iterator<T> getIterator();
+  /**
+   * Creates and returns a CustomIterator for this CustomContainer.
+   *
+   * @return A CustomIterator for this CustomContainer.
+   */
+  public Iterator<T> getIterator();
 }
