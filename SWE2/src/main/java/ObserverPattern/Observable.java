b97331e3cf7acdf48b3cb65e84ca92b57a3a85a7
paul kraft <paul.kraft@riseup.net>
2019-11-08 15:15:26 +0100

    added raw version of Observer-Pattern

---
+++ SWE2/src/main/java/ObserverPattern/Observable.java
@@ -0,0 +1,49 @@
+package ObserverPattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * DIY Observer Pattern - defines the Observable.
+ * @author Paul Kraft
+ */
+public class Observable{
+
+    //TODO: maybe logging?
+
+    /** List of all Observers that are Interested in the Data of the Observer!
+     */
+    private List<Observer> observerList = new ArrayList<>();
+
+    /** Adds a certain Observer to the observerList
+     * @param obs the Observer that should be added.
+     * @throws Exception if Observer already subscribed.
+     */
+    public void subscribe(Observer obs) throws Exception{
+        if(observerList.contains(obs))
+            throw new Exception("Observable ALREADY SUBSCRIBED to this Observer - can not subscribe!");
+        else
+            observerList.add(obs);
+    }
+
+    /** Removes a certain Observer from the observerList
+     * @param obs the Observer that should be removed.
+     * @throws Exception if Observer not subscribed at this time.
+     */
+    public void unSubscribe(Observer obs) throws Exception{
+        if(!observerList.contains(obs))
+            throw new Exception("Observable was NOT SUBSCRIBED to this Observer - can not unsubscribe!");
+        else
+            observerList.remove(obs);
+    }
+
+    /** Updates all Observers, to use if smthg. observable has changed.
+     * @param obj Object that changed, which interests the observer.
+     */
+    public void updateObservers(Object obj){
+        for(Observer obs : observerList ){
+            obs.update(obj);
+        }
+    }
+}
--- SWE2/src/main/java/persistency/FileReader.java
+++ SWE2/src/main/java/persistency/FileReader.java
@@ -7,50 +7,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileReader {
-  private String fileName;
+	private String fileName;
 
-  public FileReader(String fileName) {
-    this.fileName = fileName;
-  }
-
-  public List<Object> readFile() {
-    return new ArrayList<>();
-  }
-
-  private void Deserialize() {
-
-    File file = new File(fileName);
-
-    try {
-
-      if (file.createNewFile()) {
-        List<? extends Object> list =
-            new ArrayList<>(); // TODO if file doesnt Exist this Data ( 'list' ) gets written to
-                               // file.....!
-        FileWriter writer = new FileWriter(); // TODO
-        writer.writeFile(
-            list); // not the fastest but new File Writer only gets created if and if only file
-                   // doesnt exist, doesnt happen often!
-      }
-
-    } catch (Exception e) {
-      // TODO EXCEPTION HANDLING
+    public FileReader(String fileName) {
+        this.fileName = fileName;
     }
 
-    try {
+    public List<Object> readFile() {
+		return new ArrayList<>();
+	}
 
-      FileInputStream inputStream = new FileInputStream(fileName);
-      ObjectInputStream objinputstream = new ObjectInputStream(inputStream);
+	private void Deserialize(){
 
-      // " When read they need to be cast to the expected type. "
-      // https://docs.oracle.com/javase/9/docs/api/java/io/ObjectInputStream.html
-      List<? extends Object> returnlist = (List<? extends Object>) objinputstream.readObject();
+		File file = new File(fileName);
 
-      inputStream.close();
-      objinputstream.close();
+		try{
 
-    } catch (Exception e) {
-      // TODO EXCEPTION HANDLING
-    }
-  }
+			if(file.createNewFile()){
+                List<? extends Object> list = new ArrayList<>();
+                FileWriter writer = new FileWriter();
+                writer.writeFile(list); // not the fastest but new File Writer only gets created if and if only file doesnt exist, doesnt happen often!
+            }
+
+
+        }catch (Exception e) {
+			// TODO EXCEPTION HANDLING
+		}
+
+
+		try {
+
+
+
+			FileInputStream inputStream = new FileInputStream(fileName);
+			ObjectInputStream objinputstream = new ObjectInputStream(inputStream);
+
+			//" When read they need to be cast to the expected type. " https://docs.oracle.com/javase/9/docs/api/java/io/ObjectInputStream.html
+            List<? extends Object> returnlist = (List<? extends Object>)objinputstream.readObject();
+
+			inputStream.close();
+			objinputstream.close();
+
+		} catch (Exception e) {
+			// TODO EXCEPTION HANDLING
+		}
+
+	}
 }
