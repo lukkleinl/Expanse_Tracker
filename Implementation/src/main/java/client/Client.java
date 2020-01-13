449136ccee8ad8d5c89cf7f869bab87f7dfedb72
Lukas Kleinl <a01546221@unet.univie.ac.at>
2020-01-13 18:17:55 +0100

    updated client

--- Implementation/src/main/java/client/Client.java
+++ Implementation/src/main/java/client/Client.java
@@ -2,7 +2,6 @@ package client;
 
 
 import gui.main.UserInterface;
-import patterns.observing.Database;
 import user.User;
 
 public class Client {
@@ -12,12 +11,6 @@ public class Client {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
 
-
-    try {
-    Database localStorage = new Database();
-    }catch (Exception e){
-      System.out.println(e.getMessage() +"Client");
-    }
 
       UserInterface userInterface = new UserInterface();
       userInterface.start();
